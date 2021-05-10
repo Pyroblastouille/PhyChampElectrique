d50aa1510e4cd7817c773588378a89e40863cb46
Yannis <TheCakeIsNotALie>
2021-05-10 08:31:48 -0700

    C Bo

--- charge.h
+++ charge.h
@@ -1,17 +1,43 @@
 #ifndef CHARGE_H
 #define CHARGE_H
+#include <stdbool.h>
 #include "vector.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_render.h>
 
-
-
-typedef struct {
-    Vector pos;
+typedef struct
+{
     double q;
+    Vector pos;
 } charge_t;
-int compute_e(charge_t c, Vector p, double eps, Vector *e);
 
+//E = k * q/r^2
+
+//c_pos = particules qui créent le champ éléctrique
+//P = point random
+
+// Compute E*c_pos*P/norm(c_pos*P)
+// Return false if norm(c_pos*P) < eps
+bool compute_e(charge_t c, Vector p, double eps, Vector *e);
+
+// Compute the normalized sum of Ei*qiP/norm(qiP)
+// Return false if for some qiP, norm(qiP) < eps
+bool compute_total_normalized_e(charge_t *charges, int num_charges, Vector p,
+double eps, Vector *e);
+
+// Compute and then draw all the points belonging to a field line,
+// starting from pos0.
+// Returns false if pos0 is not a valid position
+// (for example if pos0 is too close to a charge).
+static bool draw_field_line(struct gfx_context_t *ctxt, charge_t *charges,
+int num_charges, double dx, Vector pos0, double x0,
+double x1, double y0, double y1);
+
+// Draw all the charges
+// A circle with minus sign for negative charges
+// A circle with a plus sign for positive charges
+static void draw_charges(struct gfx_context_t *context, charge_t *charges,
+int num_charges, double x0, double x1, double y0, double y1);
 
 
 #endif
