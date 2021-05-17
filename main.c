b4750453de8ebdb28d475f55dee290d6b9b56307
Gaël Mariot <gael.mariot@hesge.ch>
2021-05-17 08:59:56 -0700

    Couleurs :D

--- main.c
+++ main.c
@@ -33,8 +33,9 @@ int main()
     win = SDL_CreateWindow("Champ Electriques", 700, SDL_WINDOWPOS_CENTERED, WIN_WIDTH, WIN_HEIGHT, 0);
     screenRender = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
     double rotate = 0;
-    double rotateIncrement = PI/2400;
-
+    double rotateIncrement = PI/12000;
+    int x = 50,y = 50;
+    
     while (running)
     {
         SDL_RenderClear(screenRender);
@@ -56,9 +57,29 @@ int main()
         }
         
 
+        //Fais des trucs
+        Uint32 mouseState =   SDL_GetMouseState(&x,&y);
+        int r = 255,g=255,b=255;
+        if(mouseState & SDL_BUTTON(SDL_BUTTON_LEFT)){    
+            g = 0;
+
+        }
+        if (mouseState & SDL_BUTTON(SDL_BUTTON_RIGHT))
+        {
+            b = 0;
+        }
+        if (mouseState & SDL_BUTTON(SDL_BUTTON_MIDDLE))
+        {
+            r = 0;
+        }
+        
+        SDL_SetRenderDrawColor(screenRender, r,g,b, SDL_ALPHA_TRANSPARENT);
+        draw_charges(screenRender,charges,2,0,0,200,200,rotate);
+
         SDL_RenderPresent(screenRender);
 
         rotate += rotateIncrement;
+
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
