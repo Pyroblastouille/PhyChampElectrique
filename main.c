2eb227b08e20cca1a56b68fe72d1e91c9ed68572
Gaël Mariot <gael.mariot@hesge.ch>
2021-06-15 04:28:17 -0700

    Cest Magneufique

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ SDL_Event event;
 int main()
 {   
     double spacing = 0.01;
-    int num_charges = 3;
+    int num_charges = 4;
     charge_t charges[num_charges];
     charges[0].pos = vec_createVectorXY(CHARGE_RADIUS,WIN_HEIGHT/2);
     charges[0].q = 1;
@@ -28,6 +28,8 @@ int main()
     charges[1].q = -1;
     charges[2].pos= vec_createVectorXY(WIN_WIDTH-CHARGE_RADIUS,WIN_HEIGHT/2);
     charges[2].q = 1;
+    charges[3].pos= vec_createVectorXY(WIN_WIDTH/2,WIN_HEIGHT- CHARGE_RADIUS);
+    charges[3].q = -1;
 
     //Déclaration de la fenêtre SDL
     srand(0);
@@ -42,7 +44,6 @@ int main()
     double deltaTime = 0;
     while (running)
     {   
-
         LAST = NOW;
         NOW = SDL_GetPerformanceCounter();
 
@@ -58,12 +59,13 @@ int main()
 
         //Fais des trucs
         draw_charges(screenRender,charges,num_charges,-WIN_WIDTH,WIN_WIDTH*2,-WIN_HEIGHT,WIN_HEIGHT*2,rotate);
-        for(int j = 0; j < num_charges;j++)
-        for (int i = 0; i < LINES_NUMBER; i++) {
-            double x = charges[j].pos.x + (CHARGE_RADIUS + spacing) * cos((2*PI)/LINES_NUMBER * i);
-            double y = charges[j].pos.y + (CHARGE_RADIUS + spacing) * sin((2*PI)/LINES_NUMBER * i);
-            Vector pos0 = {.x= x, .y = y};
-            draw_field_line(screenRender, charges, num_charges, DELTA_HOP, pos0, -WIN_WIDTH,WIN_WIDTH*2,-WIN_HEIGHT,WIN_HEIGHT*2);
+        for(int j = 0; j < num_charges;j++){
+            for (int i = 0; i < LINES_NUMBER; i++) {
+                double x = charges[j].pos.x + (CHARGE_RADIUS + spacing) * cos((2*PI)/LINES_NUMBER * i);
+                double y = charges[j].pos.y + (CHARGE_RADIUS + spacing) * sin((2*PI)/LINES_NUMBER * i);
+                Vector pos0 = {.x= x, .y = y};
+                draw_field_line(screenRender, charges, num_charges, DELTA_HOP, pos0, -WIN_WIDTH,WIN_WIDTH*2,-WIN_HEIGHT,WIN_HEIGHT*2);
+            }
         }
         
 
--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #define WIN_WIDTH 1200
 #define WIN_HEIGHT 720
 #define CHARGE_RADIUS 30
-#define LINES_NUMBER 1000
+#define LINES_NUMBER 60
 #define DELTA_HOP 1
 
 
