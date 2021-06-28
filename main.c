739b288c0c559e1190ed2cca9b6431a1b38594d5
Gaël Mariot <gael.mariot@hesge.ch>
2021-06-28 05:24:41 -0700

    Comment main.c,main.h

--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
@@ -13,15 +11,15 @@
 
 SDL_Window *win = NULL;
 SDL_Renderer *screenRender = NULL;
-
 SDL_Event event;
 
 int main()
 {   
+    //Crée la graine de l'aléatoire
     time_t t;
     srand((unsigned)time(&t));
 
-    double spacing = 0.01;
+    //Crée les charges
     int num_charges = rand()%(MAX_CHARGES-MIN_CHARGES) + MIN_CHARGES;
     charge_t charges[num_charges];
     for (size_t i = 0; i < num_charges; i++)
@@ -35,41 +33,50 @@ int main()
     win = SDL_CreateWindow("Champs Electriques", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_WIDTH, WIN_HEIGHT, 0);
     screenRender = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
 
-    //Variables pour la rotation des signes
+    //Variables pour la rotation des charges
     double rotate = 0;
     double rotateIncrement = PI/4;
 
+    //Variables pour le calcul du deltaTime
     Uint64 NOW = SDL_GetPerformanceCounter();
     Uint64 LAST = 0;
     double deltaTime = 0;
+
+    //Boucle de l'application
     while (running)
     {   
+        //Calcule le deltaTime
         LAST = NOW;
         NOW = SDL_GetPerformanceCounter();
-
         deltaTime = (double)((NOW - LAST) / (double)SDL_GetPerformanceFrequency() );
 
+        //Clean l'arrière plan
         SDL_RenderClear(screenRender);
-        //met la couleur de l'arrière plan à noir
         SDL_SetRenderDrawColor(screenRender, 0, 0, 0, SDL_ALPHA_TRANSPARENT);
         SDL_RenderFillRect(screenRender, NULL);
 
+        //Définit la couleur des lignes de champs et des charges
         SDL_SetRenderDrawColor(screenRender, 255, 255, 255, SDL_ALPHA_TRANSPARENT);
 
-        //Fais des trucs
+        //Dessine les charges
         draw_charges(screenRender,charges,num_charges, LOW_WIDTH_BOUND, HIGH_WIDTH_BOUND, LOW_HEIGHT_BOUND, HIGH_HEIGHT_BOUND,rotate);
+        //Dessine les lignes de champs
         for(int j = 0; j < num_charges;j++){
             for (int i = 0; i < LINES_NUMBER; i++) {
-                double x = charges[j].pos.x + (CHARGE_RADIUS + spacing) * cos((2*PI)/LINES_NUMBER * i);
-                double y = charges[j].pos.y + (CHARGE_RADIUS + spacing) * sin((2*PI)/LINES_NUMBER * i);
+                double x = charges[j].pos.x + (CHARGE_RADIUS + SPACING) * cos((2*PI)/LINES_NUMBER * i);
+                double y = charges[j].pos.y + (CHARGE_RADIUS + SPACING) * sin((2*PI)/LINES_NUMBER * i);
                 Vector pos0 = {.x= x, .y = y};
                 draw_field_line(screenRender, charges, num_charges, DELTA_HOP, pos0, LOW_WIDTH_BOUND, HIGH_WIDTH_BOUND, LOW_HEIGHT_BOUND, HIGH_HEIGHT_BOUND);
             }
         }
-        
+
+        //Active les dessins sur le rendu
         SDL_RenderPresent(screenRender);
 
-        rotate += rotateIncrement * deltaTime;
+        //Augmente la rotation
+        rotate =  rotate + rotateIncrement * deltaTime;
+
+        //Gère les entrées utilisateurs (Pour fermer le programme)
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
--- main.h
+++ main.h
@@ -1,19 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+//Constantes de taille de fenêtre
 #define WIN_WIDTH 1200
 #define WIN_HEIGHT 720
 
+//Const Min ou Max de charges
 #define MAX_CHARGES 5
-#define MIN_CHARGES 1
+#define MIN_CHARGES 2
 
+//Constantes de taille des charges
+#define CHARGE_RADIUS 30
+
+//Constantes d'écriture des lignes de champ
+#define SPACING 0.01
 #define LOW_WIDTH_BOUND -100000
 #define HIGH_WIDTH_BOUND 100000
 #define LOW_HEIGHT_BOUND -100000
 #define HIGH_HEIGHT_BOUND 100000
-
-#define CHARGE_RADIUS 30
 #define LINES_NUMBER 60
 #define DELTA_HOP 1
+
 
 #endif
